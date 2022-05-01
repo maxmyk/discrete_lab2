78515f747c0a6c2aa9393ae8c4c7461576be5bf2
Bohdan213 <93724511+Bohdan213@users.noreply.github.com>
2022-05-01 22:42:46 +0300

    Update rsa_module.py

--- rsa_module.py
+++ rsa_module.py
@@ -23,7 +23,7 @@ class RSA():
         """
         self.__n = self.__p * self.__q
         self.__pq = (self.__p - 1) * (self.__q - 1)
-        self.__e = PrimeNumber(20)
+        self.__e = PrimeNumber(30)
         self.__d = pow(self.__e, -1, mod=self.__pq)
 
     def encrypt(self, message, key):
@@ -33,7 +33,7 @@ class RSA():
         block = 0
         while numbers_form != []:
             to_encode.append(bytearray())
-            for _ in range(0, 156):
+            for _ in range(0, 256):
                 try:
                     to_encode[block].append(numbers_form.pop(0))
                 except IndexError:
@@ -47,12 +47,12 @@ class RSA():
         message = ""
         for elem in encode_form.split(','):
             pre_chr = pow(int(elem), self.__d, self.__n)
-            pre_chr = pre_chr.to_bytes(156, 'big')
+            pre_chr = pre_chr.to_bytes(256, 'big')
             for elem in pre_chr:
                 if elem != 0:
                     message += chr(elem)
         return message
-    
+
     @property
     def e(self):
         return self.__e
